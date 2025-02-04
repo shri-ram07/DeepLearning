8f9a886d36fa258002d21dffdd8d378db133a74b
Shri Ram dwivedi <raunakd511@gmail.com>
2025-02-04 23:39:24 +0530

    Updated

--- DeepNeuralNetworkonXORgate.py
+++ DeepNeuralNetworkonXORgate.py
@@ -12,7 +12,7 @@ model = Sequential()  #Simple Sequential model
 # model.add(Dense(3, input_shape=(2,) , activation='relu'))
 #we do not need to specify the input layer , we can directly put it to input_dim of the hidden layer
 model.add(Dense(16,input_dim=2,activation='relu'))
-model.add(Dense(16,input_dim=16,activation='relu'))
+model.add(Dense(16,input_dim=16,activation='relu')) #hidden layers
 model.add(Dense(16,input_dim=2,activation='relu'))
 model.add(Dense(16,input_dim=2,activation='relu'))
 model.add(Dense(16,input_dim=2,activation='relu'))
@@ -28,13 +28,14 @@ model.add(Dense(1, activation='sigmoid'))
 #now we have to choose , how to model is going to find the
 #minimum loss function , here we use SGD(Stochastic Gradient Descent) with leaning rate 0.3
 
-sgd = keras.optimizers.SGD(learning_rate=0.3)
+#sgd = keras.optimizers.SGD(learning_rate=0.3)
 #now we will compile this SGD in our model
-model.compile(loss='mean_squared_error', optimizer=sgd)
+#we can use adam also to calculate minimum loss function -- more reliable
+model.compile(loss='mean_squared_error', optimizer="adam", metrics=['binary_accuracy'])
 
 #fit the model
-model.fit(X,Y,batch_size=4, epochs=1000000)
+model.fit(X,Y ,  epochs=10000 , verbose=2)  #verbose 0 is silent , 1 and 2 are showing results
 #batch_size means number of training samples and epochs is number of training iteration
 print("Prediction is : ")
-print(model.predict(X))
+print(model.predict(X).round())
 
